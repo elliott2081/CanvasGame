c8ea0deef962c4e302b5caebafcf2fe124586733
David Elliott <delliott@cs.uoregon.edu>
2013-03-31 18:33:25 -0700

    changed hero, maps, map16, tileMovement

--- hero.js
+++ hero.js
@@ -11,7 +11,7 @@ var heroFrameIndex = 0;
 
 var hero = {
 		speed: 256, //movementin pixels per second (original : 256)
-		x: 900, //original: 192
+		x: 192, //original: 192
 		y: 200, //original: 64
 		direction: 0,
 		ease: 10,
